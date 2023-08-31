bd569fa68219126ce8c547d6cb95804e0a0d931a
Stefan <stefan38164@gmail.com>
2023-08-31 14:49:37 +0200

    Meal app

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvelableMeals from './AvelableMeals';
+import MealsSummary from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvelableMeals />
+    </>
+  );
+};
+
+export default Meals;
